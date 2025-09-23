9305035197bc04d1d6b1c942cfa34ac4d9661254
webdevmission <webdevmission@gmail.com>
2025-09-23 12:18:47 +0200

    initial tree-file

---
+++ Tree.java
@@ -0,0 +1,10 @@
+public class Tree {
+  public static void main(String[] args) {
+//it's the starting point of every program
+System.out.println("Hi, Jamie here!");
+System.out.println("Planting a tree.");
+System.out.println("  *  ");
+System.out.println(" *** ");
+System.out.println("*****");
+  }
+}
